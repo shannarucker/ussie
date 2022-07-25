0f68ab458f320bfedd70ea2d4aa58553d3649cfa
Shanna Rucker <42379382+shannarucker@users.noreply.github.com>
2022-07-25 15:38:12 -0400

    test matches

--- tests/testthat/test-matches.R
+++ tests/testthat/test-matches.R
@@ -1,3 +1,15 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("uss_make_matches works", {
+
+  # use the function
+  italy <- uss_make_matches(engsoccerdata::italy, "Italy")
+
+  expect_true(tibble::is_tibble(italy))
+  expect_named(
+    italy,
+    c("country", "tier", "season", "date", "home", "visitor",
+      "goals_home", "goals_visitor")
+  )
+  expect_identical(unique(italy$country), "Italy")
+
+  expect_s3_class(italy$tier, "factor")
 })
